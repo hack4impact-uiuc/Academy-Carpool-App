2e53bc6f8231637b1b341c532273433a5120c7b8
sahimuth <sahithi.muthyala@gmail.com>
2019-11-04 23:36:45 -0600

    changes2

--- backend/api/Trips.py
+++ backend/api/Trips.py
@@ -2,15 +2,16 @@ from mongoengine import *
 from flask_mongoengine import document
 from .base import db
 from api.core import Mixin
+from api.core import User, Car, Location
 
 class Trip(Document, Mixin):
-    driver = ReferenceField(User, required = true)
-    origin = ReferenceField(Location, required = true)
-    destination = ReferenceField(Location, required = true)
+    driver = ReferenceField("User", required = true)
+    origin = ReferenceField("Location", required = true)
+    destination = ReferenceField("Location", required = true)
     start_time = StringField(required = true)
-    posted_time = StringField(required = true)
+    posted_time = DateTimeField(required = true)
     cost = StringField(required = true)
-    car = ReferenceField(Car, required = true)
+    car = ReferenceField("Car", required = true)
     seats_available = IntField(required = true)
     trunk_space = StringField(required = true)
-    passengers = ListField(ReferenceField(User, required = true))
+    passengers = ListField(ReferenceField("User", required = true))
