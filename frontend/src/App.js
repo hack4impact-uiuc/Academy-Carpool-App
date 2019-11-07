237976706301eb19421723307c2e9767cacc5a8f
Angela Luo <ael49021@gmail.com>
2019-11-06 21:57:43 -0600

    hi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TripComponent from './TripComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      orgin:"Shlopkins", 
+      destination: "Naperthrill", 
+      date: "October 10", 
+      time: "5:00 pm",
+      price: "5",
+      name: "Angela Luo",
+      seats: 5,
+      trunk: "Large",
+      notes: "Line up at the start. The running speed starts slowly but gets faster each minute after you hear this signal bodeboop. A sing lap should be completed every time you hear this sound. ding Remember to run in a straight line and run as long as possible. "
+  }
+  }
+  render () {
+    return (
+      
+      <container> 
+            <TripComponent 
+            orgin = {this.state.orgin} 
+            destination = {this.state.destination} 
+            date = {this.state.date} 
+            time = {this.state.time}
+            price = {this.state.price}
+            name = {this.state.name}
+            trunk = {this.state.trunk}
+            seats = {this.state.seats}
+            notes = {this.state.notes}
+            />
+              
+      </container>
+            
+      
+
+    );
+  }
 }
 
 export default App;
