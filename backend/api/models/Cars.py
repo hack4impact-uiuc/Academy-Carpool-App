ec7d93babb09df4b6ec234614d7078a7ea3ed7f5
mattwalo32 <mattwalowski@gmail.com>
2019-11-06 21:49:27 -0600

    Create user enpoints

--- backend/api/models/Cars.py
+++ backend/api/models/Cars.py
@@ -1,5 +1,5 @@
 from mongoengine import *
-from flask_mongoengine import document
+from flask_mongoengine import Document
 from .base import db
 from api.core import Mixin
 
@@ -7,7 +7,10 @@ class Cars(Document, Mixin):
     year = IntField(required = False)
     model = StringField(required = True)
     color = StringField(required = True)
-    license_plate = StringField(required = True)
+    license_plate = StringField(required = True, max_length=4)
 
     def __repr__(self):
         return f"<Car {self.license_plate}>"
+
+def getRequiredKeys():
+    return ["model", "color", "license_plate"]
