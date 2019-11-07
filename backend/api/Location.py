969c1faab2e071fcb1d95fcb21be1b623c1a1e0b
sahimuth <sahithi.muthyala@gmail.com>
2019-11-06 22:04:32 -0600

    update_with_black

--- backend/api/Location.py
+++ backend/api/Location.py
@@ -3,7 +3,8 @@ from flask_mongoengine import document
 from .base import db
 from api.core import Mixin
 
+
 class Location(Document, Mixin):
-    name = StringField(required = true)
-    latitude = FloatField(required = true)
-    longitude = FloatField(required = true)
+    name = StringField(required=true)
+    latitude = FloatField(required=true)
+    longitude = FloatField(required=true)
