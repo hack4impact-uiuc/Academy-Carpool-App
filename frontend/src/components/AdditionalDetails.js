48539f35ffa8ad2ebaad7f0296e165e035da54b4
ashankbehara <abehara2@illinois.edu>
2019-11-13 23:25:54 -0600

    All formatting changes

--- frontend/src/components/AdditionalDetails.js
+++ frontend/src/components/AdditionalDetails.js
@@ -15,8 +15,10 @@ import {
   Toast,
   ToastBody,
   ToastHeader,
-  Badge
+  Badge,
+  Container
 } from 'reactstrap';
+import MapContainer from './MapContainer';
 class AdditionalDetails extends React.Component {
   render() {
     return (
@@ -41,17 +43,25 @@ class AdditionalDetails extends React.Component {
                 @ <b>{this.props.details.time}</b> on <b>{this.props.details.date}</b>
               </div>
               <br />
-              <div>
-                <b>Car Model: </b>
-                {this.props.details.make} {this.props.details.model} <br />
-                <b>Color: </b> {this.props.details.color} <br />
-                <b>License Plate: </b> {this.props.details.plate} <br />
-                <b>Seats Available: </b> {this.props.details.seats}
-                <br />
-                <b>Trunk Space: </b> {this.props.details.space}
-                <br />
-                <b>Special Instructions: </b> {this.props.details.notes} <br />
-              </div>
+              <Container>
+                <div>
+                  <b>Car Model: </b>
+                  {this.props.details.make} {this.props.details.model} <br />
+                  <b>Color: </b> {this.props.details.color} <br />
+                  <b>License Plate: </b> {this.props.details.plate} <br />
+                  <b>Seats Available: </b> {this.props.details.seats}
+                  <br />
+                  <b>Trunk Space: </b> {this.props.details.space}
+                  <br />
+                  <b>Special Instructions: </b> {this.props.details.notes} <br />
+                </div>
+                <iframe
+                  className="map"
+                  src={`https://maps.google.com/maps?q=${this.props.details.destination}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
+                >
+                  <MapContainer style={{ float: 'center' }} />
+                </iframe>
+              </Container>
             </CardBody>
           </Card>
         </div>
