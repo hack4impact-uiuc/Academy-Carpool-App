450f8fff0f502430715e1f888517b6b8dce7f0c5
Angela Luo <ael49021@gmail.com>
2019-11-13 23:24:42 -0600

    gucci

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TripComponent from './TripComponent';
+import TripFormPage from './TripFormPage';
+import LocationSearchInput from './PlacesAutocomplete';
+import Map from './Map';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <LocationSearchInput/> */}
+      {/* <Map/>  */}
+      <TripFormPage />
     </div>
   );
 }
