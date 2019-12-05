3ddc6c2e728a226cdeb48853cdd8acb26b51c441
Angela Luo <ael49021@gmail.com>
2019-12-04 22:49:06 -0600

    applied yarn format

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,30 @@
 import Homepage from './pages/Homepage.js';
 import './App.css';
-import TripComponent from './TripComponent';
+import React, { Component } from 'react';
+// import TripComponent from './components/TripComponent';
 import TripFormPage from './TripFormPage';
-import LocationSearchInput from './PlacesAutocomplete';
-import Map from './Map';
+// import LocationSearchInput from './PlacesAutocomplete';
+// import Map from './Map';
+//import UserFormPage from './pages/UserFormPage'
+import HomePage from './pages/Homepage';
+import BookTripComponent from './components/BookTripComponent';
 
 function App() {
   return (
     <div>
       {/* <LocationSearchInput/> */}
       {/* <Map/>  */}
-      <TripFormPage />
+      {/* <TripFormPage /> */}
+      <BookTripComponent
+        origin="Chambana"
+        destination="Chicagooo"
+        name="Navam"
+        date="November 40, 2018"
+        time="12:00 pm"
+        visible={true}
+      />
     </div>
   );
-
 }
 
 export default App;
--- frontend/src/components/BookTripComponent.js
+++ frontend/src/components/BookTripComponent.js
@@ -3,14 +3,14 @@ import { Card, Row, Col } from 'react-bootstrap';
 import { MDBIcon } from 'mdbreact';
 import { TimePicker, DatePicker, InputNumber, Input, Select, Form, Button, Modal } from 'antd';
 
-// origin, destination, name, date, time, and whether it should be visible or not 
+// origin, destination, name, date, time, and whether it should be visible or not
 //set visibility to true when the Book Now! link from the TripComponent is pressed
 class BookTripComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       visible: this.props.visible
-    }
+    };
   }
 
   handleSubmit = e => {
@@ -29,122 +29,118 @@ class BookTripComponent extends React.Component {
 
   showModal = () => {
     this.setState({
-      visible: true,
+      visible: true
     });
   };
 
   handleOk = e => {
     console.log(e);
     this.setState({
-      visible: false,
+      visible: false
     });
   };
 
   handleCancel = e => {
     console.log(e);
     this.setState({
-      visible: false,
+      visible: false
     });
   };
 
-
   render() {
     const { getFieldDecorator } = this.props.form;
-    return ( 
+    return (
       <div>
         <Modal
-        title="Basic Modal"
-        visible= {this.state.visible}
-        onOk={this.handleOk, this.handleSubmit}
-        onCancel={this.handleCancel}
+          title="Basic Modal"
+          visible={this.state.visible}
+          onOk={(this.handleOk, this.handleSubmit)}
+          onCancel={this.handleCancel}
         >
-        <Card.Body>
-        <Row>
-          <Col xs={12} md={5}>
-            <Card.Title size = "small">{this.props.details.origin.location.name}</Card.Title>
-          </Col>
-          <Col xs={12} md={2}>
-            <Card.Title>
-              <MDBIcon icon="arrow-right" />{' '}
-            </Card.Title>
-          </Col>
-          <Col xs={6} md={5}>
-            <Card.Title size = "small"> {this.props.details.destination.name} </Card.Title>
-          </Col>
-        </Row>
-        <Row  >
-          <Col md="auto">
-            <Card.Subtitle> {this.props.details.driver.name}</Card.Subtitle>
-          </Col>
-          <Col md="auto">
-            <Card.Subtitle> {this.props.details.start_date}</Card.Subtitle>
-          </Col>
-          <Col md="auto">
-            <Card.Subtitle> {this.props.details.start_time}</Card.Subtitle>
-          </Col>
-        </Row>
-        <Form>
-                <div className="top-buffer">
-                  <Row>
-                    <Col>
-                      <Form.Item>
-                        {getFieldDecorator('firstName', {
-                          rules: [{ required: true, message: 'Please enter first name' }]
-                        })(<Input placeholder="First name" size="large" value={this.state.name} />)}
-                      </Form.Item>
-                    </Col>
-                    <Col>
-                      <Form.Item>
-                        {getFieldDecorator('lastName', {
-                          rules: [{ required: true, message: 'Please enter last name' }]
-                        })(<Input placeholder="Last name" size="large" />)}
-                      </Form.Item>
-                    </Col>
-                  </Row>
-                </div>
-
-                <Form.Item>
-                        {getFieldDecorator('email', {
-                          rules: [{ required: true, message: 'Please enter email' }]
-                        })(<Input placeholder="Email" size="large" />)}
-                </Form.Item>
-
+          <Card.Body>
+            <Row>
+              <Col xs={12} md={5}>
+                <Card.Title size="small">{this.props.details.origin.location.name}</Card.Title>
+              </Col>
+              <Col xs={12} md={2}>
+                <Card.Title>
+                  <MDBIcon icon="arrow-right" />{' '}
+                </Card.Title>
+              </Col>
+              <Col xs={6} md={5}>
+                <Card.Title size="small"> {this.props.details.destination.name} </Card.Title>
+              </Col>
+            </Row>
+            <Row>
+              <Col md="auto">
+                <Card.Subtitle> {this.props.details.driver.name}</Card.Subtitle>
+              </Col>
+              <Col md="auto">
+                <Card.Subtitle> {this.props.details.start_date}</Card.Subtitle>
+              </Col>
+              <Col md="auto">
+                <Card.Subtitle> {this.props.details.start_time}</Card.Subtitle>
+              </Col>
+            </Row>
+            <Form>
+              <div className="top-buffer">
                 <Row>
-                    <Col>
-                      <Form.Item >
-                      {getFieldDecorator('phone', {
-                          rules: [{ required: true, message: 'Please input your phone number!' }],
-                      })(<Input size = "large" placeholder = "Phone #" addonBefore={'+1'} style={{ width: '100%' }} />)}
-                      </Form.Item>
+                  <Col>
+                    <Form.Item>
+                      {getFieldDecorator('firstName', {
+                        rules: [{ required: true, message: 'Please enter first name' }]
+                      })(<Input placeholder="First name" size="large" value={this.state.name} />)}
+                    </Form.Item>
+                  </Col>
+                  <Col>
+                    <Form.Item>
+                      {getFieldDecorator('lastName', {
+                        rules: [{ required: true, message: 'Please enter last name' }]
+                      })(<Input placeholder="Last name" size="large" />)}
+                    </Form.Item>
+                  </Col>
+                </Row>
+              </div>
 
-                    </Col>
-                    <Col>
-                      <Form.Item>
-                        {getFieldDecorator('venmo', {
-                          rules: [{ required: true, message: 'Please enter venmo' }]
-                        })(<Input placeholder="Venmo Handle" size="large" />)}
-                      </Form.Item>
-                    </Col>
-                  </Row>
+              <Form.Item>
+                {getFieldDecorator('email', {
+                  rules: [{ required: true, message: 'Please enter email' }]
+                })(<Input placeholder="Email" size="large" />)}
+              </Form.Item>
 
+              <Row>
+                <Col>
                   <Form.Item>
-                      {getFieldDecorator('age', {
-                          rules: [{ required: true, message: 'Please enter age' }]
-                      })(
-                          <InputNumber placeholder = "Age" size="large" min={1} max={120} defaultValue={18}/>
-                      )}
+                    {getFieldDecorator('phone', {
+                      rules: [{ required: true, message: 'Please input your phone number!' }]
+                    })(<Input size="large" placeholder="Phone #" addonBefore={'+1'} style={{ width: '100%' }} />)}
                   </Form.Item>
-
+                </Col>
+                <Col>
                   <Form.Item>
-                  {getFieldDecorator('notes', {
-                    rules: [{ required: false, message: 'Optional notes not entered' }]
-                  })(<Input placeholder="Optional notes to driver:" size="large" />)}
+                    {getFieldDecorator('venmo', {
+                      rules: [{ required: true, message: 'Please enter venmo' }]
+                    })(<Input placeholder="Venmo Handle" size="large" />)}
                   </Form.Item>
-              </Form>
+                </Col>
+              </Row>
+
+              <Form.Item>
+                {getFieldDecorator('age', {
+                  rules: [{ required: true, message: 'Please enter age' }]
+                })(<InputNumber placeholder="Age" size="large" min={1} max={120} defaultValue={18} />)}
+              </Form.Item>
+
+              <Form.Item>
+                {getFieldDecorator('notes', {
+                  rules: [{ required: false, message: 'Optional notes not entered' }]
+                })(<Input placeholder="Optional notes to driver:" size="large" />)}
+              </Form.Item>
+            </Form>
           </Card.Body>
         </Modal>
-        </div>
-    )
+      </div>
+    );
   }
 }
 const WrappedForm = Form.create()(BookTripComponent);
